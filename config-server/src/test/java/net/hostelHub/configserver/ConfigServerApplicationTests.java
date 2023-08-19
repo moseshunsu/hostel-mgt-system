18f57521cfe29e2fe8c0e10df5665560a69c2463
Moses Hunsu <hunsumoses7@gmail.com>
2023-08-19 10:36:09 +0100

    Initial commit for the config server microservice

---
+++ config-server/src/test/java/net/hostelHub/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.hostelHub.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
