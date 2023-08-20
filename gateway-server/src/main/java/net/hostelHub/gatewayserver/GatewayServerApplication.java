00ef3ef3778d836455b31e87a7bc8476b3acac62
Moses Hunsu <hunsumoses7@gmail.com>
2023-08-20 22:33:15 +0100

    Made miscellaneous changes

--- gateway-server/src/main/java/net/hostelHub/gatewayserver/GatewayServerApplication.java
+++ gateway-server/src/main/java/net/hostelHub/gatewayserver/GatewayServerApplication.java
@@ -9,10 +9,6 @@ import reactor.netty.http.client.HttpClient;
 public class GatewayServerApplication {
 
 	public static void main(String[] args) {
-        HttpClient client =
-                HttpClient.create().resolver(DefaultAddressResolverGroup.INSTANCE);
-//        HttpClient.create().resolver(DefaultAddressResolverGroup.INSTANCE);
-
 		SpringApplication.run(GatewayServerApplication.class, args);
 	}
 
