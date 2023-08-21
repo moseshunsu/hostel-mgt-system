9eae2c0d252d1bafe591b4f2eac8f974978aefef
Moses Hunsu <hunsumoses7@gmail.com>
2023-08-21 19:01:51 +0100

    First commit for the room management service

---
+++ room-mgt-service/src/main/java/net/HostelHub/roommgtservice/RoomMgtServiceApplication.java
@@ -0,0 +1,13 @@
+package net.HostelHub.roommgtservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomMgtServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomMgtServiceApplication.class, args);
+	}
+
+}
