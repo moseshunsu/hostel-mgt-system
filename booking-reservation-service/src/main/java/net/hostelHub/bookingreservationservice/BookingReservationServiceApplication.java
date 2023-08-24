20b8f643a47a9e8904f53d0726668126fde68ea7
Moses Hunsu <hunsumoses7@gmail.com>
2023-08-24 08:33:53 +0100

    Initial commit for the booking & reservation service

---
+++ booking-reservation-service/src/main/java/net/hostelHub/bookingreservationservice/BookingReservationServiceApplication.java
@@ -0,0 +1,13 @@
+package net.hostelHub.bookingreservationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingReservationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingReservationServiceApplication.class, args);
+	}
+
+}
---
+++ booking-reservation-service/src/test/java/net/hostelHub/bookingreservationservice/BookingReservationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.hostelHub.bookingreservationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingReservationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
